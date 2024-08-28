123c6f7048aaf57cc0b68bf32f27464c55d7bebe
lukashe14 <lukas.heino@metropolia.fi>
2024-08-28 09:09:16 +0300

    moduuli 3

---
+++ Tehtävä 1.py
@@ -0,0 +1,4 @@
+kuha = float(input("Anna kuhan pituus:"))
+lasku = 37.00 - kuha
+if kuha < 37.00:
+    print("Kuha on ali-mittainen. Laske se takaisin järveen. Kuhasta puuttuu", lasku,"cm verran pituutta.")
